a9e88029e4c5ddd0f5c3dd3f23280e45c3991858
Eduardo Nienkotter Marcelino <110943998+dudunm@users.noreply.github.com>
2025-04-17 19:27:45 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/exercicio20/Exercicio20.java
@@ -0,0 +1,8 @@
+package com.mycompany.exercicio20;
+public class Exercicio20 {
+    public static void main(String[] args) {
+        for(int num = 1; num < 11; num++ ){
+            System.out.println(num);
+        } 
+    }
+}
